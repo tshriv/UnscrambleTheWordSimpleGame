482c6601263b30befe99e7a4ceedb2d57c8ef033
tshriv <tshrivas15@gmail.com>
2022-04-12 22:30:07 +0530

    updating

---
+++ app/src/main/java/com/game/newUnscrambleApp/view/MainFragment.kt
@@ -0,0 +1,38 @@
+package com.game.newUnscrambleApp.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.game.newUnscrambleApp.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnStartGame: Button = requireActivity().findViewById<Button>(R.id.btn_start_game)
+        btnStartGame.setOnClickListener {
+            findNavController().navigate(
+                MainFragmentDirections
+                    .actionMainFragmentToGameFragment()
+            )
+        }
+    }
+}
